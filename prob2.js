a6e029b2c975f1d998b7dedaecf90f28cd792281
Hitesh <sharmahitesh9828@gmail.com>
2023-08-30 11:12:04 +0530

    add js prob

---
+++ prob2.js
@@ -0,0 +1,15 @@
+//Define a variable called name with your Name as the assigned value
+//Print the value stored in the variable name
+//Change the variable to store your father's name
+//Print the value stored in the variable name
+//Change the variable again to store your mother's name
+//Print the value stored in the variable name
+
+let name = "hitesh sharma"
+console.log(name)
+
+name = "pradeep sharma"
+console.log(name)
+
+name = "rekha sharma"
+console.log(name)
---
+++ prob4.js
@@ -0,0 +1,22 @@
+//prob 4
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+//Print the report card of the student (You can make it look nice by using some keyboard symbols )
+//Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ //(Links to an external site.)) for some inspiration
+
+console.log("😂😂😂😂😂 RESULT 😂😂😂😂😂")
+
+let name = "jethalal champaklal gada 👈"
+let school = "bhachao senior secondary , guj."
+let section = "sigma male section"
+let rollno = "1"
+let education = "kacchi vyapari"
+
+console.log("name =",name)
+console.log("school =",school)
+console.log("section =",section)
+console.log("rollno. =",rollno)
+console.log("education =",education)
+
+console.log("😒😒😒 marks obtained 😒😒😒")
+console.log("history   geography  economics")
+console.log("   12         26         99")
