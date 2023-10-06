42c56b5f2c13d80dedef3e36dfafa28ca018e8ec
Hitesh <sharmahitesh9828@gmail.com>
2023-10-06 20:42:45 +0530

    modify

--- fetch 2/weather 1/index.js
+++ fetch 2/weather 1/index.js
@@ -12,31 +12,32 @@ try {
     let iconID=data.weather[0].icon;
     iconurl=`https://openweathermap.org/img/wn/${iconID}.png`
     icon.src=iconurl;
-
+    
+   icon.style.backgroundColor=" rgb(188, 26, 188)"
     let temp=document.getElementById("temp");
-    temp.textContent=data.main.temp;
+    temp.textContent=`Temprature -${data.main.temp}`;
         let mintemp=document.getElementById("mintemp");
-        mintemp.textContent=data.main.temp_min;
+        mintemp.textContent=`Min Temp -${data.main.temp_min}`;
     let maxtemp=document.getElementById("maxtemp");
-    maxtemp.textContent=data.main.temp_max;
+    maxtemp.textContent=`${data.main.temp_max}`;
     let pressure=document.getElementById("pressure");
-    pressure.textContent=data.main.pressure;
+    pressure.textContent=`pressure -${data.main.pressure}`;
     let windspeed=document.getElementById("windspeed");
-    windspeed.textContent=data.wind.speed;
+    windspeed.textContent=`Wind Speed-${data.wind.speed}`;
     let rise=document.getElementById("rise");
-    rise.textContent=new Date(
-        data.sys.sunrise *1000
-    ).toLocaleTimeString();
+    rise.textContent=`Sun Rise -${new Date(
+       data.sys.sunrise *1000
+    ).toLocaleTimeString()}`
        let set=document.getElementById("set");
-       set.textContent=new Date(
+       set.textContent=`Sun Set -${new Date(
         data.sys.sunset *1000
-       ).toLocaleTimeString();
+       ).toLocaleTimeString()}`
     let cloud=document.getElementById("cloud");
-    cloud.textContent=data.clouds.all;
+    cloud.textContent=`Clouds -${data.clouds.all}`;
 
     let map=document.getElementById("Map");
     map.src=`https://maps.google.com/maps?q=${city}&t=&z=13&ie=UTF8&iwloc=&output=embed`;
-    
+    map.className="dis"
 
 } catch (error) {
     console.log(error)
